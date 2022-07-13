3be873a3de3421882c311cbf2c6e99268056bc00
Kaustubh <kaustubh.kesarkar@gmail.com>
2022-07-13 13:11:07 +0530

    Added kafka send code with callback.

--- src/main/kotlin/com/experiments/kafka/tutorial01/ProducerDemo.kt
+++ src/main/kotlin/com/experiments/kafka/tutorial01/ProducerDemo.kt
@@ -3,20 +3,39 @@ package com.experiments.kafka.tutorial01
 import com.experiments.kafka.utils.KafkaServerProperties
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.slf4j.LoggerFactory
 
 class ProducerDemo {
 }
 
 fun main(args: Array<String>) {
+    val logger = LoggerFactory.getLogger(ProducerDemo::class.java)
+
     println("---- Producer Demo ----")
 
     val producer = KafkaProducer<String, String>(KafkaServerProperties.loadProducerConfig())
 
-    // create producer record
-    val record = ProducerRecord<String, String>("dummy-java-topic", "hello java2")
+    for (i in 1..20) {
+        // create producer record
+        val record = ProducerRecord<String, String>("dummy-java-topic", "hello java $i")
 
-    // send data -async
-    producer.send(record)
+        // send data -async
+        producer.send(record) { recordMetadata: RecordMetadata, exception: Exception? ->
+            // executes every time a record is successfully sent or an exception is thrown
+            if (exception == null) {
+                logger.info(
+                    "Receive new metadata: \n" +
+                            "Topic: ${recordMetadata.topic()} \n" +
+                            "Partition: ${recordMetadata.partition()} \n" +
+                            "Offset: ${recordMetadata.offset()} \n" +
+                            "Timestamp: ${recordMetadata.timestamp()} \n"
+                )
+            } else {
+                logger.error("Error occurred while producing", exception)
+            }
+        }
+    }
 
     // flush data
     producer.flush()
